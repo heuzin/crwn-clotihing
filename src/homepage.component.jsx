4c424c415899b144ee76c644cef6876b45dfb4d8
heuzin <matheusqs.arq@gmail.com>
2021-03-14 20:29:30 -0300

    other items in homepage

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -13,6 +13,22 @@ const HomePage = () => (
         <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
+                    <h1 className='title'>JACKETS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SNEAKERS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
                     <h1 className='title'>WOMENS</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
