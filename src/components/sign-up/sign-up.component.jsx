9faf1086a8eacb342991e31781267b90d4b374fc
heuzin <matheusqs.arq@gmail.com>
2021-03-18 23:40:46 -0300

    css styles for sign up component

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './sign-up.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButtom from '../custom-button/custom-button.component'
 import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
