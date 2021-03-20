ae98682324bd21bab48a9d3e66ce8860aefe9e63
heuzin <matheusqs.arq@gmail.com>
2021-03-19 21:52:28 -0300

    tyles for cart icon an adding the component into header component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
+import './cart-icon.styles.scss';
+
 const CartIcon = () => (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon'/>
