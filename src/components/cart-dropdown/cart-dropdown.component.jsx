8c219acdfd9d7e6106626ba757792c2d9ac44bbe
heuzin <matheusqs.arq@gmail.com>
2021-03-19 22:25:14 -0300

    styles for cart dropdown and adding component into header component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import CustomButton from '../custom-button/custom-button.component';
 
+import './cart-dropdown.styles.scss';
+
 const CartDropdown = () => (
     <div className='cart-dropdown'>
         <div className='cart-items'/>
