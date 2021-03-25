864180f47259148db72cca34eccf06ff676833d4
heuzin <matheusqs.arq@gmail.com>
2021-03-25 14:31:49 -0300

    importing chheckout imte component into checkout page

--- src/components/checkout-item/checkout-tem.component.jsx
+++ src/components/checkout-item/checkout-tem.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const CheckoutItem = () => (
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => (
     <div className='checkout-item'>
         <div className='image-container'>
-            <img alt='item'/>
+            <img src={imageUrl} alt='item'/>
         </div>
-        <span className='name'></span>
-        <span className='quantity'></span>
-        <span className='price'></span>
+        <span className='name'>{name}</span>
+        <span className='quantity'>{quantity}</span>
+        <span className='price'>{price}</span>
         <div className='remove-button'>&#10005;</div>
     </div>
 )
