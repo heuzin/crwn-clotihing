7a430343c6a556e7d5b4249b7953e7e3b0389267
heuzin <matheusqs.arq@gmail.com>
2021-03-27 22:54:57 -0300

    category page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverView from '../../components/collections-overview/collections-overview.component';
+import CategoryPage from '../catergory/category.component';
 
 const ShopPage = ({ match }) => (
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionsOverView} />
+    
     </div>
 );
 
