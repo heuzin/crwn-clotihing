86ddb006cc17660190299553e028fb06cd61477f
heuzin <matheusqs.arq@gmail.com>
2021-03-27 22:48:10 -0300

    styles for collections overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 import { selectCollections } from '../../redux/shop/shop.selectors';
 
+import './collections-overview.styles.scss';
 
 const CollectionsOverView = ({ collections }) => (
     <div className='collections-overview'>
