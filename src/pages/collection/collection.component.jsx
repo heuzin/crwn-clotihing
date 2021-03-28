e6cb2bdac40d41c2592154a9607a5388d331f9fa
heuzin <matheusqs.arq@gmail.com>
2021-03-27 23:09:44 -0300

    selector for selection a collection, connectin collection component to state to get collections array which match collectionId

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 const CollectionPage = ({ match }) => (
     <div className='category'>
@@ -8,4 +11,8 @@ const CollectionPage = ({ match }) => (
     </div>
 )
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
