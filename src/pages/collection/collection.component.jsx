5e5c2bcd22f461f4f847c4d4a60c1bcfa50c4613
heuzin <matheusqs.arq@gmail.com>
2021-03-30 21:05:57 -0300

    fixing naming errors

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -10,10 +10,10 @@ import './collection.styles.scss';
 const CollectionPage = ({ collection }) => {
     const { title, items } = collection
     return (
-        <div className='category'>
-            <h2 className='ttle'>{title}</h2>
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
             <div className='items'>
-                {items.map(tem => (
+                {items.map(item => (
                     <CollectionItem key={item.id} item={item}/>
                 ))
                 }
