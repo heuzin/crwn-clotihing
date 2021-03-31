bc1ce114051764a895e5118e6b9303d8046fbe67
heuzin <matheusqs.arq@gmail.com>
2021-03-31 19:01:17 -0300

    date experation for warning message

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -38,7 +38,7 @@ const CheckoutPage = ({ cartItems, total}) => (
         <div className='test-warning'>
             *Please use the following test crediit card for payments*
             <br />
-            4242 4242 4242 4242 - Exp: 01/20 - CVW: 123
+            4242 4242 4242 4242 - Exp: Date in future - CVW: 123
         </div>
         <StripeCheckoutButton price={total}/>
     </div>
