a265843aa97c0f2dbe79edb4b5be23717d841b98
heuzin <matheusqs.arq@gmail.com>
2021-04-05 14:08:38 -0300

    reduce method to go over collections and set its tiitle as key

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -62,8 +62,11 @@ export const convertCollectionsSnapshotToMap = (collections) => {
         }
     })
 
-    console.log(transformedCollection)
-}
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    } , {});
+};
 
 firebase.initializeApp(config);
 
