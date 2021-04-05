f736096158cdddfacdd71e976d3c467918e8f4dd
heuzin <matheusqs.arq@gmail.com>
2021-04-05 12:19:50 -0300

    collection added to firebase, removing code to only be added once

--- src/App.js
+++ src/App.js
@@ -11,17 +11,16 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import { selectCollectionsForPreview } from './redux/shop/shop.selectors'
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const {setCurrentUser, collectionsArray} = this.props
+    const {setCurrentUser} = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -36,7 +35,6 @@ class App extends React.Component {
       } else {
         setCurrentUser(userAuth)
       }
-        addCollectionAndDocuments('collections', collectionsArray)
     });
   }
 
@@ -71,7 +69,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionsForPreview
 })
 
 const mapDispatchToProps = (dispatch) => ({
