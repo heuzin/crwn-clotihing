a3f8266f33310cd36849ff5f07281a9f36333b55
heuzin <matheusqs.arq@gmail.com>
2021-04-05 22:39:13 -0300

    actions for fetching collections

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,29 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+export const fetchCollectoinsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectoinsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
     payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
 })
+
+export const fetchCollectoinsStartAsync = () => {
+    return disptach => {
+        const collectionRef = firestore.collection('collections');
+        disptach(fetchCollectoinsStart())
+
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+            disptach(fetchCollectoinsSuccess(collectionsMap))            
+        }).catch(error => disptach(fetchCollectionsFailure(error.message)));
+    }
+};
