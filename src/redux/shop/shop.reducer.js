8c3063b073caf34db0e79ec6a0f2a5a3ab107c11
heuzin <matheusqs.arq@gmail.com>
2021-04-05 22:30:55 -0300

    implementing fetch api call into reducer for fetching collectoin

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,15 +1,29 @@
 import ShopActionTypes from './shop.types';
 
 const INITIAL_STATE = {
-    collections: null
+    collections: null,
+    isFetchiing: false,
+    errorMessage: undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case ShopActionTypes.UPDATE_COLLECTIONS:
+        case ShopActionTypes.FETCH_COLLECTIONS_START:
             return {
                 ...state,
+                isFetchiing: true
+            };
+        case ShopActionTypes.FETCH_COLLECTIONS_SUCCESS:
+            return {
+                ...state,
+                isFetchiing: false,
                 collections: action.payload
+            };
+        case ShopActionTypes.FETCH_COLLECTIONS_FAILURE:
+            return {
+                ...state,
+                isFetchiing: false,
+                errorMessage: action.payload
             }
         default:
             return state;
