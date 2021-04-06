8f98de86fdee0ca5d917933a30c9ea2567aeddac
heuzin <matheusqs.arq@gmail.com>
2021-04-05 22:07:52 -0300

    worknig on spinner error

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
 
-const WithSpinner = WrappedComponent => ({ isLoading, ...props }) => {
-    return isLoading ? (
-        <SpinnerOverlay>
-            <SpinnerContainer />
-        </SpinnerOverlay>
-    ) : (
-        <WrappedComponent {...props}/>
-    )
+const WithSpinner = WrappedComponent => {
+    const Spinner = ({ isLoading, ...props }) => {
+        return isLoading ? (
+            <SpinnerOverlay>
+                <SpinnerContainer />
+            </SpinnerOverlay>
+        ) : (
+            <WrappedComponent {...props}/>
+        )
+    };
+    return Spinner
 };
 
 export default WithSpinner;
