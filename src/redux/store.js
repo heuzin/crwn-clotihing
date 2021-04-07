ece3a6d5b379f134e2d87cd26d2857c5d85ab4e0
heuzin <matheusqs.arq@gmail.com>
2021-04-06 21:06:13 -0300

    complete project until redux thunk implementation

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk'
 
 import rootReducer from './root-reducer';
 
-const sagaMidleware = createSagaMiddleware();
-
-const middlewares = [sagaMidleware];
+const middlewares = [thunk];
 
 if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger)
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
-// sagaMidleware.run()
 
 export const persistor = persistStore(store);
