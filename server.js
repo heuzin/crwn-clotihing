13b0ca99e3ccc8c07c84479d867eb43eea83c269
heuzin <matheusqs.arq@gmail.com>
2021-04-14 23:40:14 -0300

    express.static and app.listen

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
@@ -8,3 +7,20 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const app = express;
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.length('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+};
+
+app.listen(port, error => {
+    if (error) throw error;
+    console.log('server running on port ' + port)
+});
