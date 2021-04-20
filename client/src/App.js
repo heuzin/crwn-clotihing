ae3db1ebdd1fbaa7a5f7042b2496fa0e48eac9e0
heuzin <matheusqs.arq@gmail.com>
2021-04-20 20:39:06 -0300

    lazy loading shop page components

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import { checkUserSession } from './redux/user/user.actions';
 const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
 const ShopPage = lazy(() => import('./pages/shop/shop.component'));
 const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
-const CheckoutPage = lazy(() => ('./pages/checkout/checkout.component.jsx'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component.jsx'));
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
